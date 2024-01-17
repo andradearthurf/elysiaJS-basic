bebb0e42ad7d2fb8f1a8f2cba3c8766e72b6f311
Arthur <andradearthurf@gmail.com>
2024-01-17 18:05:57 -0300

    Updates with new available routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
+import { MoviesDatabase } from './database';
 
-const app = new Elysia()
+const app = new Elysia().decorate('db', new MoviesDatabase());
 
-app.get("/", () => "Hello Elysia")
+app.post('/movies', ({ db, body }) => db.addMovie(body), {
+  body: t.Object({
+    title: t.String(),
+    director: t.String(),
+  }),
+});
+app.get('/movies', ({ db }) => db.getMovies());
 
-app.listen(3000)
+app.get('/movies/:id', ({ db, params }) => db.getMovie(parseInt(params.id)));
+
+app.put(
+  '/movies',
+  ({ db, body }) =>
+    db.updateMovie(body.id, {
+      title: body.title,
+      director: body.director,
+    }),
+  {
+    body: t.Object({
+      id: t.Number(),
+      title: t.String(),
+      director: t.String(),
+    }),
+  }
+);
+
+app.delete('/movies/:id', ({ db, params }) =>
+  db.deleteMovie(parseInt(params.id))
+);
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-)
+);
